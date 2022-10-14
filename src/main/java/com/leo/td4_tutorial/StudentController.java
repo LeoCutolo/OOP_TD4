1c9e4b4ca1c29400eaa243949e3f201395eff72d
Leo <leocutolo@gmail.com>
2022-10-14 12:33:45 +0200

    LCU 2022/10/14: Adding Display to students and link to the controller in fxml

--- src/main/java/com/leo/td4_tutorial/StudentController.java
+++ src/main/java/com/leo/td4_tutorial/StudentController.java
@@ -48,9 +48,19 @@ public class StudentController implements Initializable {
         List<Student> hardStudents = new ArrayList<>();
         hardStudents.add(new Student(1,"lolo","female"));
         hardStudents.add(new Student(2,"lili","male"));
+
         ObservableList<Student> students= FXCollections.observableArrayList(hardStudents);
         lvStudentList.setItems(students);
 
+        lvStudentList.getSelectionModel().selectedItemProperty().addListener(e->
+        displayStudentDetails(lvStudentList.getSelectionModel().getSelectedItem()));
 
+    }
+
+    private void displayStudentDetails(Student selectedStudent) {
+        if(selectedStudent!=null){
+            txtName.setText(selectedStudent.getName());
+            cmbGender.setValue(selectedStudent.getGender());
+        }
     }
 }
